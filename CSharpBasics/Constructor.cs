2558ef7d9e1e3b9367d3c94b3c5e0a37017819e7
SabitaCSIT <SabitaCSIT2074_30@mbmc.edu.np>
2020-09-23 21:40:55 +0545

    learned about static constructor.

--- CSharpBasics/Constructor.cs
+++ CSharpBasics/Constructor.cs
@@ -1,37 +1,44 @@
 namespace LearningClasses
 {
+
     public class Country
     {
         public string Name { get; set; }
+        public int Popultion { get; set; }
 
-        public int Population { get; set; }
+        public static bool isOnEarth;
 
-        //Default constructor : parameter less
+        //Instance constructor
+        //Default constructor - parameterless
         public Country()
         {
 
         }
-
-        //Parametrized constructor
-
-        public Country(string name)
-        {
-            Name = name;
-        }
+        //  constructer with parameters
         public Country(string name, int population)
         {
             Name = name;
-            Population = population;
+            Popultion = population;
         }
 
+        //ststic constructor
+        static Country()
+        {
+
+        }
     }
 
     public class Demo
     {
         void DoSomething()
         {
-            Country country1=new Country();
-            Country country2=new Country("Nepal");
+            Country country1 = new Country();
+            Country country2 = new Country("Nepal", 123456);
+            var a = country2.Name;
+
+            // static members can be called with class name itself
+            var x = Country.isOnEarth;
+
 
         }
     }
