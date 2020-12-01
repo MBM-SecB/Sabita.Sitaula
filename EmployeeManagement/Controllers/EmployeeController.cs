8ad2473ec82326f2a654e1c9c651436f6162e0f5
SabitaCSIT <SabitaCSIT2074_30@mbmc.edu.np>
2020-12-01 17:27:01 +0545

    Learned about querying db

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,20 +1,25 @@
 using System.Linq;
+using EmployeeManagement.Data;
 using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
-   
+    private EMSContext db;
+
+    public EmployeeController(EMSContext _db)
+    {
+        db = _db;
+    }
+
     public ActionResult Index()
     {
-        var employees = Employee.GetEmployees();
+        var employees = db.Employees.ToList();
         return View(employees);
     }
 
-    public ActionResult Detail(string firstName)
+    public ActionResult Detail(int id)
     {
-        var employees = Employee.GetEmployees();
-        var employee = employees.FirstOrDefault(x => x.FirstName == firstName);
-        
+        var employee = db.Employees.Find(id);
         return View(employee);
     }
 
@@ -24,6 +29,21 @@ public class EmployeeController : Controller
     }
 
     [HttpPost]
-   public ActionResult<bool> Add(Employee employee) => true;    //Model binding
-}
+    public ActionResult Add(Employee employee)  // Model binding
+    {
+        db.Employees.Add(employee);
+        db.SaveChanges();
 
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public ActionResult Delete(int id)
+    {
+        var employee = db.Employees.Find(id);
+        db.Employees.Remove(employee);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -3,6 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 public class Employee
 {
+    public int Id { get; set; }
+
     [Required(ErrorMessage="We need First name")]
     [Display(Name = "First Name")]
     public string FirstName { get; set; }
@@ -14,59 +16,4 @@ public class Employee
     public char Gender { get; set; } = 'F';
     public double Salary { get; set; }
 
-
-public static List<Employee> GetEmployees()
-    {
-        //Object initializer
-        Employee employee1 = new Employee()
-        {
-            FirstName = "Sabita", 
-            Surname = "Sitaula", 
-            Address = "Jhapa",
-            Salary = 100.00
-        };
-
-        Employee employee2 = new Employee
-        { 
-            FirstName = "Binita", 
-            Surname = "subedi", 
-            Address = "Ktm",
-            Salary = 10000.00
-        };
-
-        Employee employee3 = new Employee
-        {
-            FirstName = "Aniss", 
-            Surname = "Ghimire", 
-            Address = "Bhaktapur",
-            Salary = 100.00
-        };
-
-        Employee employee4 = new Employee
-        { 
-            FirstName = "Sabita1", 
-            Surname = "Sitaula", 
-            Address = "Jhapa",
-            Salary = 100.00
-        };
-
-        Employee employee5 = new Employee
-        { 
-            FirstName = "Sabita2", 
-            Surname = "Sitaula", 
-            Address = "Jhapa",
-            Salary = 100.00
-        };
-
-        Employee employee6 = new Employee
-        { 
-            FirstName = "Sabita3", 
-            Surname = "Sitaula", 
-            Address = "Jhapa",
-            Salary = 100.00
-        };
-
-        var employees = new List<Employee>() {employee1, employee2, employee3, employee4, employee5, employee6};
-        return employees;
-    }
 }
